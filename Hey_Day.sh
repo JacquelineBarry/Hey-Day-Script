78c9b2bf054d32eb9d5cc75a4c3f8e9f9e6b85fe
Jacqueline Barry <71234775+JacquelineBarry@users.noreply.github.com>
2020-10-13 19:06:47 -0500

    Added file.

---
+++ Hey_Day.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#The user is prompted to type their name as input
+echo "Enter your name"
+
+#The user input is assigned to the variable "name"
+read name 
+
+#The user's name is printed as the variable following a greeting!
+echo "Hey $name"
+
